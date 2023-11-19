7e486cbbb3419af23e1b9fc6563fa411ee992847
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2023-11-20 00:35:20 +0100

    MakefileAutoPush

--- cuba/main.c
+++ cuba/main.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 19:45:35 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/19 23:34:57 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/11/20 00:35:19 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void 	draw_cub(t_data *game, int size , int _x, int _y)
 	{
 		while (x < size)
 		{
-			mlx_put_pixel(game->img, _x+x , _y+y, 0x0FFFFFF);
+			mlx_put_pixel(game->img, _x + x , _y + y, 0x0FFFFFF);
 			x++;
 		}
 		x = 0;
@@ -29,26 +29,40 @@ void 	draw_cub(t_data *game, int size , int _x, int _y)
 	}
 }
 
-void	draw_map(t_data *game)
+ // proportional to the size of the map and move the map with keys
+// instead of the player
+void	draw_map(t_data *game, int size ,int _x, int _y)
 {
+	int x = 0;
+	int y = 0;
 
-	draw_cub(game,20,game->mlx->width - 50,game->mlx->height-50);
+	int moves_x = 0;
+	int moves_y = 0;
+
+	while (game->map[y])
+	{
+		while (game->map[y][x])
+		{
+			if (game->map[y][x] == '1')
+				draw_cub(game,size,_x+x+moves_x,_y+y+moves_y);
+			if (game->map[y][x] == game->player_info.direction)
+				draw_cub(game,2,_x+x+moves_x,_y+y+moves_y); // center the dot 
+			x++;
+			moves_x += size;
+		}
+		moves_x = 0;
+		x = 0;
+		y++;
+		moves_y += size;
+	}
+}
+
+void	my_drawing(t_data *game)
+{
+	mlx_delete_image(game->mlx, game->img);
+	game->img = mlx_new_image(game->mlx, game->mlx->width, game->mlx->height);
+	draw_map(game,20,20,20); // cant resize to a minimun set and maximum set
 	mlx_image_to_window(game->mlx, game->img, 0, 0);
-
-	// int x = 0;
-	// int y = 0;
-
-	// while (game->map[y])
-	// {
-	// 	while (game->map[y][x])
-	// 	{
-	// 		if (game->map[y][x] == '1')
-	// 			draw_cub(game,20,x*50,y*50);
-	// 		x++;
-	// 	}
-	// 	x = 0;
-	// 	y++;
-	// }
 }
 
 int	main(int ac, char **av)
@@ -57,21 +71,20 @@ int	main(int ac, char **av)
 	
     if (ac != 2)
 		return (ft_fprintf(2, RED "Error : supply the map file.\n" DEFAULT), 1);
-	// game = (t_data *)ft_calloc(sizeof(t_data), 1);
-    // if (!game)
-	// {
-    // 	ft_fprintf(2, "memory allocation failed.");
-	// 	exit(1);
-	// }
+	parser(&game, read_file(*(++av)));
     // init_images(game);
     // open_window(game);
-	parser(&game, read_file(*(++av)));
+
+// 	mlx_focus(mlx_t* mlx);
+// void mlx_get_monitor_size(int32_t index, int32_t* width, int32_t* height);
+
+	
 	game.mlx = mlx_init(WIDTH, HEIGHT, "cuba", true);
-	game.img = mlx_new_image(game.mlx, game.mlx->width, game.mlx->height);
+	
 
 
 	// mlx_key_hook(game.mlx, ft_key, game);
-	mlx_loop_hook(game.mlx, (void *)draw_map, &game);
+	mlx_loop_hook(game.mlx, (void *)my_drawing, &game);
 
 	mlx_loop(game.mlx);
 	mlx_terminate(game.mlx);
