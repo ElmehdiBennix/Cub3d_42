5ca8fae807eb5e2aa1435418a380ed8521aeecec
El Mehdi Bennix <ebennix@e2r2p7.1337.ma>
2023-11-21 02:34:59 +0100

    merge and drawing phase

--- cuba/parse/Interpreter.c
+++ cuba/parse/Interpreter.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:49:33 by ebennix           #+#    #+#             */
-/*   Updated: 2023/11/21 02:32:42 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/11/21 02:34:33 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,26 +41,26 @@ void 	boundary_check(t_data *game)
 	int j = 0;
 
 	if (adbdoul_lewel(game->map[i]) == false)
-		return (ft_fprintf(2,"Error : map must be surrounded by walls."), exit(1));
+		return (ft_fprintf(2,"Error : map must be surrounded by walls."), free2d(game->map), exit(1));
 	while (game->map[++i] && (unsigned int)i < game->map_height - 1)
 	{
 		if (game->map[i][0] == '0')
-			return (ft_fprintf(2,"Error : map must be surrounded by walls."), exit(1));
+			return (ft_fprintf(2,"Error : map must be surrounded by walls."), free2d(game->map), exit(1));
 		while (game->map[i][j])
 		{
 			if (game->map[i][j] == '0' || game->map[i][j] == game->player_info.direction)
 			{
 				if (plus_check(game, i + 1, j) == false || plus_check(game, i - 1, j) == false ||
 						plus_check(game, i, j + 1) == false || plus_check(game, i, j - 1) == false)
-					return (ft_fprintf(2,"Error : map must be surrounded by walls."), exit(1));
+					return (ft_fprintf(2,"Error : map must be surrounded by walls."), free2d(game->map), exit(1));
 			}
 			j++;
 		}
 		j = 1;
 	}
 	if (adbdoul_lewel(game->map[i]) == false)
-		return (ft_fprintf(2,"Error : map must be surrounded by walls."), exit(1));
-}
+		return (ft_fprintf(2,"Error : map must be surrounded by walls."), free2d(game->map), exit(1));
+} // free map and textures
 
 //#######################//#######################//#######################//#######################
 
