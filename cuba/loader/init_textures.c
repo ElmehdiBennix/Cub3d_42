a4fbe03fdb75efc0554eed0d7abd796041bc3b74
El Mehdi Bennix <ebennix@e1r2p15.1337.ma>
2023-12-04 04:05:07 +0100

    almost finished

--- cuba/loader/init_textures.c
+++ cuba/loader/init_textures.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 20:29:40 by ebennix           #+#    #+#             */
-/*   Updated: 2023/12/04 03:57:07 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/12/04 04:04:46 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void load_image_to_screen(t_data *game, char *content ,mlx_image_t **img_adrs ,i
         return (free2d(game->map), free_texture(game) ,exit(1));
     *img_adrs = mlx_texture_to_image(game->mlx, texs);
     if (!(*img_adrs) || mlx_image_to_window(game->mlx, *img_adrs, cordenates[0], cordenates[1]) == -1)
-        return (free2d(game->map), free_texture(game) ,exit(1));
+        return (free2d(game->map), free_texture(game) ,ft_error());
     mlx_delete_texture(texs);
 }
 
@@ -53,7 +53,7 @@ void    init_images(t_data *game)
 {
     game->canvas.world_3D = mlx_new_image(game->mlx, WIDTH, HEIGHT);
     if (!game->canvas.world_3D ||mlx_image_to_window(game->mlx, game->canvas.world_3D, 0, 0) == -1)
-        return(free2d(game->map), free_texture(game) ,exit(1));
+        return(free2d(game->map), free_texture(game) ,ft_error());
     load_image_to_screen(game ,"./assets/textures/gun/gun1.png", &game->canvas.gun[0], (int32_t[2]){624, 270});
     load_image_to_screen(game ,"./assets/textures/gun/gun2.png", &game->canvas.gun[1], (int32_t[2]){624, 270});
     load_image_to_screen(game ,"./assets/textures/gun/gun3.png", &game->canvas.gun[2], (int32_t[2]){624, 270});
